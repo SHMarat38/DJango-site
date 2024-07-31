9c057fef73c381cb4d68870ee9e71f4bc7f29d9a
SHMarat38 <itprogitprog@gmail.com>
2024-07-31 11:09:49 +0400

    Конец 9

--- itprog/news/views.py
+++ itprog/news/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import News 
+from .forms import NewsForm
 def news_home(request):
     news = News.objects.order_by('-date')
     return render(request, 'main/news_home.html', {'news' :news})
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/news')
+        else :
+            error = 'Форма была не верной'
+    form = NewsForm()
+    data = {
+        'form' : form,
+        'error': error
+    }
+    return render(request, 'main/create.html', data)
